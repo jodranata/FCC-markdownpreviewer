1b8f81ed60bcf3347c1a37a7712385276989f118
Jodi Pranata <jodranata@gmail.com>
2020-02-05 16:55:40 +0700

    Updates

---
+++ precache-manifest.06d2319e022a8e5fa8cf4df56127ade0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9173034d87cddf22464f8efbbd38dd31",
+    "url": "/FCC-markdownpreviewer/index.html"
+  },
+  {
+    "revision": "8118716e5c1fdfd86329",
+    "url": "/FCC-markdownpreviewer/static/css/main.46e7caba.chunk.css"
+  },
+  {
+    "revision": "481d9790bca5aa81776b",
+    "url": "/FCC-markdownpreviewer/static/js/2.6dcc127f.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/FCC-markdownpreviewer/static/js/2.6dcc127f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8118716e5c1fdfd86329",
+    "url": "/FCC-markdownpreviewer/static/js/main.9864bdee.chunk.js"
+  },
+  {
+    "revision": "c8ee40589523fe84fbed",
+    "url": "/FCC-markdownpreviewer/static/js/runtime-main.2c5abf22.js"
+  }
+]);
